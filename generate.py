8fc172cbee4aa0e4f5b35b0939451fc2bccfbe03
Lucas Depetris <lucasdepetris14@gmail.com>
2024-05-16 15:38:32 -0300

    upgrade generate.py

--- generate.py
+++ generate.py
@@ -1,18 +1,39 @@
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
-from pyotp import TOTP
-import base64
 import hashlib
+import base64
+from Crypto.Cipher import AES
 
+SEED = ''
+UUID = ''
+DEVICE_ID = ''
+
+SERIAL = 'TOKENSERIALunknown'
 
 # fill these with values explained in README.md
 android_ssaid = b''
-seed = b''
+seed = b""
 
-key = hashlib.sha256(android_ssaid + b'unknownnull').digest()
-aes = AES.new(key, AES.MODE_CBC, b'\x00'*16)
-data = unpad(aes.decrypt(base64.b64decode(seed)), 8)
-data = base64.b32encode(bytes.fromhex(data.decode('utf-8')))
+def unpad(s):
+    return s[0:-ord(s[-1])]
 
-totp = TOTP(data, interval = 60)
-print(totp.now())
+def decrypt(cipher, key):
+    sha256 = hashlib.sha256()
+    sha256.update(bytes(key, 'utf-8'))
+    digest = sha256.digest()
+    iv = bytes([0] * 16)
+    aes = AES.new(digest, AES.MODE_CBC, iv)
+    decrypted = aes.decrypt(base64.b64decode(cipher))
+    return unpad(str(decrypted, "utf-8"))
+
+uuid_key = DEVICE_ID + SERIAL[11:]
+print("UUID KEY: %s" % uuid_key)
+decoded_uuid = decrypt(UUID, uuid_key)
+print("UUID: %s" % decoded_uuid)
+
+seed_decryption_key = uuid_key + decoded_uuid
+print("SEED KEY: %s" % seed_decryption_key)
+decrypted_seed = decrypt(SEED, seed_decryption_key)
+
+totp_secret = bytes.fromhex(decrypted_seed)
+
+totp_secret_encoded = str(base64.b32encode(totp_secret), "utf-8")
+print("TOTP SECRET: %s" % totp_secret_encoded)
